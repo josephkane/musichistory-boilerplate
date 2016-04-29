8cee829bb9fdb2d59d19cd4ccc6ff4d635e0b6b5
Joseph Kane <josephckane@gmail.com>
2016-04-28 21:19:15 -0500

    mid-attempt on delete button for each row. event listeners not working

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ function dataCall () {
 
 // assign data that was pulled in via XHR to a variable
 	var data = JSON.parse(this.responseText);
+	console.log(data);
+	console.log(data.songs.length);
 
+	// create a variable to use to populate the DOM with the data you get from the XHR
 	var songInfo = "";
 
 	for (var i = 0; i < data.songs.length; i++) {
@@ -14,15 +17,24 @@ function dataCall () {
 		songInfo += "<h2>" + data.songs[i].song + "</h2>";
 		songInfo += "<ul><li>" + data.songs[i].artist + "</li>";
 		songInfo += "<li class='album'>" + data.songs[i].album + "</li>";
-		songInfo += "<li>" + data.songs[i].genre + "</li></ul></div";
+		songInfo += "<li>" + data.songs[i].genre + "</li>";
+		songInfo += "<li><button class='deleteButton buttons'>Delete</button></li></ul></div>";
+	};
+
+	var deleteButtons = songSelector.getElementsByClassName("deleteButton");
+
+	for (var j = 0; j < deleteButtons.length; j++) {
+		deleteButtons[j].addEventListener("click", function() {
+			songInfo.innerHTML = "";
+		});
+	};
+
+	console.log(deleteButtons);
+	console.log(typeof deleteButtons[0]);
 
 	songList.innerHTML = songInfo;
-	}
 
-	console.log(data);
-	console.log(data.songs.length);
-	console.log(songInfo);
-}
+};
 
 var dataRequest = new XMLHttpRequest();
 
